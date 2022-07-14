e657a4222bf0afff845f71b045372bac111add18
Arif Eker <eng.arifeker@gmail.com>
2022-07-14 13:14:52 +0300

    step 5
    Rigidbody ve Collider çarpışmaları scriptten yakalandı.

---
+++ Assets/All_Chapters/ch_9/Scripts/physic_2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class physic_2 : MonoBehaviour
+{
+
+    // 1 -> Rigidbody collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "ball_2")
+        {
+            Debug.Log("Rigidbody!");
+        }
+        else
+        {
+            Debug.Log("Rigidbody");
+        }
+    }
+
+
+    // 2 -> Collider trigger collision
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "ball_2")
+        {
+            Debug.Log("Collider!");
+        }
+        else
+        {
+            Debug.Log("Collider");
+        }
+    }
+
+}
