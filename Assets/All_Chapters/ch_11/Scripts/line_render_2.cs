43b5eb6f17a3e3ecfefc4ed4b03b9c5e81482ad2
Arif Eker <eng.arifeker@gmail.com>
2022-07-18 12:04:34 +0300

    step 11
    trail render,line render. projector ve halo efektleri denendi.

---
+++ Assets/All_Chapters/ch_11/Scripts/line_render_2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class line_render_2 : MonoBehaviour
+{
+
+    LineRenderer line_render;
+
+    void Start()
+    {
+        line_render = gameObject.AddComponent<LineRenderer>();
+        
+        line_render.material = new Material(Shader.Find("Sprites/Default"));
+        line_render.useWorldSpace = false;
+
+        line_render.startColor = Color.blue;
+        line_render.endColor = Color.red;
+
+        line_render.startWidth = .1f;
+        line_render.endWidth = .01f;
+
+        line_render.numCapVertices = 15;
+    }
+
+
+    void Update()
+    {
+
+        line_render.SetPosition(0, transform.forward);
+        line_render.SetPosition(1, transform.forward * 10f);
+
+    }
+}
