b260c698dee5ca618d2b056022b72c722178f640
Arif Eker <eng.arifeker@gmail.com>
2022-07-19 03:29:39 +0300

    step 13
    UI işlemleri yapıldı. Image-Button-Toggle-Slider gibi objelerde denemeler yapıldı.

---
+++ Assets/All_Chapters/ch_12/Scripts/canvas_2.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvas_2 : MonoBehaviour
+{
+    Image fish_img;
+
+    void Start()
+    {
+        fish_img = GameObject.Find("fish").GetComponent<Image>();
+
+
+        /*
+         * fish_img.fillMethod:
+         * 
+         Horizontal (0)	 :  The Image will be filled Horizontally.
+         Vertical	(1) :  The Image will be filled Vertically.
+         Radial90	(2) :  The Image will be filled Radially with the radial center in one of the corners.
+         Radial180	(3) :  The Image will be filled Radially with the radial center in one of the edges.
+         Radial360	(4) :  The Image will be filled Radially with the radial center at the center.
+        */
+
+        fish_img.fillMethod = 0;
+
+    }
+
+
+    void Update()
+    {
+
+        if (fish_img.fillAmount > .5f)
+        {
+            fish_img.fillAmount -= .1f * Time.deltaTime;
+        }
+
+    }
+
+
+    public void MyButtonClick()
+    {
+        print("Button Click");
+    }
+
+    public void SayYourNumber(float number)
+    {
+        print("Your number is : " + number);
+    }
+
+
+    public void SexChange(bool status)
+    {
+        if (status)
+        {
+            print("Sex: Male");
+        }
+        else
+        {
+            print("Sex: Female");
+        }
+    }
+
+}
---
+++ Assets/All_Chapters/ch_12/Scripts/canvas_3.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvas_3 : MonoBehaviour
+{
+
+    public Slider slider;
+    void Start()
+    {
+        slider.minValue = 0;
+        slider.maxValue = 200;
+        slider.wholeNumbers = true;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    //public void ShowSliderValue()
+    //{
+    //    print(slider.value);
+    //}
+
+    public void ShowSliderValue(float val)
+    {
+        print(val);
+    }
+}
