960a5be1ced0e0808c14fb58d2e387ed6fb39144
Arif Eker <eng.arifeker@gmail.com>
2022-07-24 12:18:08 +0300

    step 17: object pooling
    Object pooling işlemi yapıldı. Obje herhangi bir kamera görüş açısından çıktığında pasif hale getiriliyor. Scene ekranındaki kamera da buna dahil. Eğer scene ekrandan objeleri düşerken takip edersek pasifleşmezler.

---
+++ Assets/All_Chapters/ch_18/Scripts/object_pooling_1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class object_pooling_1 : MonoBehaviour
+{
+    public List<GameObject> game_objects;
+    public Transform start_position;
+    int index = 0;
+    void Start()
+    {
+        StartCoroutine(MoveObject());
+    }
+
+    IEnumerator MoveObject()
+    {
+        while (index<game_objects.Count)
+        {
+            game_objects[index].transform.position = start_position.position;
+            game_objects[index].SetActive(true);
+            game_objects[index].AddComponent<object_visible_1_1>();
+            index = (index + 1) % game_objects.Count;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+}
---
+++ Assets/All_Chapters/ch_18/Scripts/object_visible_1_1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class object_visible_1_1 : MonoBehaviour
+{
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+        Destroy(this);
+    }
+}
