846e9ac39ee60c6c4a426622e5f582115452e9b2
Arif Eker <eng.arifeker@gmail.com>
2022-07-24 14:22:15 +0300

    step 18: mini map
    oyunlardaki minimap mantığının temel hali yapıldı.

---
+++ Assets/All_Chapters/ch_18/Scripts/character_motion_2_1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_motion_2_1 : MonoBehaviour
+{
+    float moveH;
+    float moveV;
+    float jump;
+    float rotX;
+
+    private void FixedUpdate()
+    {
+        moveH = Input.GetAxis("Horizontal");
+        moveV = Input.GetAxis("Vertical");
+        jump = Input.GetAxis("Jump");
+
+        transform.Translate(new Vector3(moveH, jump, moveV) * 5 * Time.deltaTime);  
+    }
+}
---
+++ Assets/All_Chapters/ch_18/Scripts/game_control_2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_control_2 : MonoBehaviour
+{
+
+    public GameObject character;
+
+    private void Awake()
+    {
+        character.AddComponent<character_motion_2_1>();
+    }
+}
