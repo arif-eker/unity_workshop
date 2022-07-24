b1073d35c4610478f9513084098e22afdfddbe07
Arif Eker <eng.arifeker@gmail.com>
2022-07-24 19:09:53 +0300

    step 19: loading screen
    temel anlamda bir loading geçiş ekranı yapıldı. burada asenkron sahne yüklendi ve progress çubuğu güzelce yapıldı.

---
+++ Assets/All_Chapters/ch_18/Scripts/load_scene_3.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class load_scene_3 : MonoBehaviour
+{
+    public GameObject loadingScreenPanel;
+    public Slider progressBar;
+    public Text progressText;
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(SceneLoadProgress(sceneName));
+    }
+
+
+    IEnumerator SceneLoadProgress(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+        loadingScreenPanel.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            //float progress = Mathf.Clamp01(operation.progress) +.1f;
+            progressBar.value = (operation.progress + .1f)  * 100;
+            progressText.text = "Loading (%" + (progressBar.value) + ")";
+
+            if (operation.progress >= 0.9f)
+            {
+                Debug.Log("Press the space bar to continue");
+                if (Input.GetKeyDown(KeyCode.Space))
+                    operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+
+
+}
